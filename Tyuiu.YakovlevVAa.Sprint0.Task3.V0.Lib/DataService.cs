29b7702151fbb5510bdc5f7052ef828eabb904dd
Виктор Яковлев <Tyuiu.YakovlevVA@outlook.com>
2024-09-15 14:49:07 +0500

    Task3

---
+++ Tyuiu.YakovlevVAa.Sprint0.Task3.V0.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Tyuiu.YakovlevVAa.Sprint0.Task3.V0.Lib
+{
+    public class DataService
+    {
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
