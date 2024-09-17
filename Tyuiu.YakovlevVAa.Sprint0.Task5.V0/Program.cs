30549b48a8f82122bf74ed6e2d747d0583011ef1
Виктор Яковлев <Tyuiu.YakovlevVA@outlook.com>
2024-09-17 23:11:53 +0500

    Task5

--- Tyuiu.YakovlevVAa.Sprint0.Task5.V0/Program.cs
+++ Tyuiu.YakovlevVAa.Sprint0.Task5.V0/Program.cs
@@ -8,7 +8,7 @@ namespace Tyuiu.YakovlevVAa.Sprint0.Task5.V0
             Console.WriteLine("A + B =" + DataService.Addition(1, 5));
             Console.WriteLine("A - B =" + DataService.Subtraction(15, 5));
             Console.WriteLine("A * B =" + DataService.Multiplication(10, 5));
-            Console.WriteLine(("A / B=" + DataService.Division(5, 0)));
+            Console.WriteLine("A / B=" + DataService.Division(5, 0));
             Console.ReadKey();
         }
     }
